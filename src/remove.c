1bdce68074728946843b97db1b66a954362ad9b8
Eliott Belinguier <eliott.belinguier@epitech.eu>
2021-06-13 01:39:05 +0200

    [fix] Infinite loop in the delete list function.

--- src/remove.c
+++ src/remove.c
@@ -20,12 +20,14 @@ static void list_remove_node(list_t *list_ptr, list_node_t *node)
 
 int llistlib_list_remove(list_t *list_ptr, void *element)
 {
-    list_node_t *current;
+    list_t prev = 0;
+    list_t current;
 
     if (!list_ptr || !element)
         return -1;
-    for (current = *list_ptr; current; current = current->next) {
-        if (current->element == element) {
+    for (current = *list_ptr; current && (!prev || current != *list_ptr);
+    prev = current, current = list_node_next(current)) {
+        if (list_get(current) == element) {
             list_remove_node(list_ptr, current);
             return 0;
         }
